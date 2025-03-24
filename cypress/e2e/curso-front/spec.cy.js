1c77d060882f2d8c91d2eddd435969341cdc926b
lucas-lcs <la.devtech@gmail.com>
2025-03-23 21:25:34 -0300

    add initial test

--- cypress/e2e/curso-front/spec.cy.js
+++ cypress/e2e/curso-front/spec.cy.js
@@ -2,6 +2,12 @@
 
 describe("Teste Front End", () => {
     it("Acessando a pagina", () => {
-        cy.visit("https://adopet-frontend-cypress.vercel.app/");
+        cy.visit("https://adopet-frontend-cypress.vercel.app/")
+        cy.contains("a", "Cadastrar").click()
+        cy.get('[data-test="input-name"]').type("Teste 1234")
+        cy.get('[data-test="input-email"]').type("la@gmail.com")
+        cy.get('[data-test="input-password"]').type('HlucasAlves1')
+        cy.get('[data-test="input-confirm-password"]').type('HlucasAlves1')
+        cy.get('[data-test="submit-button"]')
     })
 })
