75e3f633bd55a27526c3de20a7d755bd0279c214
LucassAllves <lhalves1@useniu.com.br>
2025-04-01 16:10:24 -0300

    test(report): Adicionando relatorio do teste.

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/separate-reports',
+    overwrite: false,
+    html: false,
+    json: true,
+    inline: true
+    // timestamp: "mmddyyyy_HHMMss"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
     },
-    video: true,
   },
-});
+})
